374ebfa68c5915df4a7db426f2a0ee99bb4c0deb
Peter Pang <pang0144@e.ntu.edu.sg>
2018-12-18 13:25:41 +0800

    [minor] set max-age for CORS preflight to reduce unnecessary API calls
    for firefox (24h) ,chrome (10 min), safari (5 min):
    - READ: one time preflight
    - CREATE: one time preflight
    - UPDATE: one preflight per object
    - DELETE: one preflight per object
    
    reference:
    - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Max-Age
    - https://bugs.chromium.org/p/chromium/issues/detail?id=131368

--- etc/python-flask-server/__main__.py
+++ etc/python-flask-server/__main__.py
@@ -6,6 +6,6 @@ from flask_cors import CORS
 from swagger_server import encoder
 
 app = connexion.App(__name__, specification_dir='./swagger/')
-CORS(app.app) 
+CORS(app.app, max_age=86400) 
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={'title': 'API Server'})
